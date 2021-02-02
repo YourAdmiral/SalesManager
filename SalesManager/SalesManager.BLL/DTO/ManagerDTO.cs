4608de9213e2a7df2e073bf336b7b39daf8195b6
MANDOM <yourmarshall@yandex.by>
2021-02-02 19:39:45 +0300

    Add DTO

---
+++ SalesManager/SalesManager.BLL/DTO/ManagerDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesManager.BLL.DTO
+{
+    public class ManagerDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<SaleDTO> Sales { get; set; }
+    }
+}
---
+++ SalesManager/SalesManager.BLL/DTO/SaleDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesManager.BLL.DTO
+{
+    public class SaleDTO
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ClientName { get; set; }
+        public int Cost { get; set; }
+        public DateTime Date { get; set; }
+        public int ManagerId { get; set; }
+    }
+}
